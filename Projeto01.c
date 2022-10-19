0cbadd0b862918782bfb9ea4ac1decfcd85a7712
João Victor Bessa de Paula <jvbpaula@gmail.com>
2022-10-19 10:00:12 -0300

    vars added

--- Projeto01.c
+++ Projeto01.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
 
-int main()
+int main(void)
 {
     
-    int num1;
+    setlocale(LC_ALL, "Portuguese");
+
+    int size;
+
+    do
+    {
+        printf("Quantos usuários serão inseridos?: ");
+        scanf("%d", &size);
+
+    } while (size < 1 || size > 1000);
     
-    printf("Teste");
+        
+
+    int id[size];
+    char nome[size], email[size], sexo[size], endereco[size];
+    double altura;
+    bool vacina;
+
 
     return 0;
 }
