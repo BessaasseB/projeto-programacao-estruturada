491046593f4751180baed026e662071a2175f2c5
João Victor Bessa de Paula <jvbpaula@gmail.com>
2022-10-26 13:22:45 -0300

    step 2

--- Projeto01.c
+++ Projeto01.c
@@ -20,11 +20,15 @@ Vacina (tomou a vacina? boolean)
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <time.h>
 
 int main(void)
 {
     
-    int size, i;
+    srand(time(NULL));
+
+    int size, i, j=0, check = 0;
 
     do
     {
@@ -36,15 +40,53 @@ int main(void)
         
 
     int id[size][1];
-    char nome[size][30], email[size][30], sexo[size][15], endereco[size][50];
+    char nome[size][30], email[size][30], sexo[size][15], endereco[size][50], vax;
     double altura[size][1];
     bool vacina[size][5];
 
+    fflush(stdin);
+
     for(i=0; i<size; i++){
-        printf("Teste\n");
+
+        id[i][i] = rand() % 999+1; 
+
+        printf("Informe o nome do usuario ID-%d: ", id[i][i]);
+        fgets(nome[i], 30, stdin);
+   
+        printf("Informe o email do usuario: ");
+        fgets(email[i], 30, stdin);
+ 
+
+        printf("Informe o sexo do usuario: ");
+        fgets(sexo[i], 30, stdin);
+
+        printf("Informe o endereco do usuario: ");
+        fgets(endereco[i], 30, stdin);
+
+        printf("Informe a altura do usuario: ");
+        scanf("%f", &altura);
+
+        fflush(stdin);
+
+        printf("O usuario esta vacinado?: ");
+        scanf("%c", &vax);
+
+        fflush(stdin);
+        
+        printf("\n");
+    }
+
+    printf("\n");
+
+    for(i=0; i<size; i++){
+        printf("%s", nome[i]); 
+        printf("%s", email[i]);
+        printf("%s", sexo[i]);
+        printf("%s", endereco[i]);  
+
+        printf("\n");
     }
 
     
-
     return 0;
 }
