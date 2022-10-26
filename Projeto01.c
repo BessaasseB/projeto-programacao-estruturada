ffc1254f4f87c2ba509f9ef915cf20248244d4ce
João Victor Bessa de Paula <jvbpaula@gmail.com>
2022-10-26 15:54:46 -0300

    step 3

--- Projeto01.c
+++ Projeto01.c
@@ -9,12 +9,12 @@ faça backup dos usuários cadastrados;
 faça restauração dos dados;
 Dados do usuário:
 
-Id (int) => preenchido automaticamente por números randômicos
-Nome completo (string)
-Email (string) => validação do campo: verificar se o caractere "@" aparece
+Id (int) => preenchido automaticamente por números randômicos CHECK
+Nome completo (string) CHECK
+Email (string) => validação do campo: verificar se o caractere "@" aparece CHECK
 Sexo (string) => validação do campo: aceitar somente as palavras Feminino, Masculino e Não declarar
-Endereço (string)
-Altura (double) => validação do campo: aceitar valores entre 1 e 2 m.
+Endereço (string) CHECK
+Altura (double) => validação do campo: aceitar valores entre 1 e 2 m. CHECK
 Vacina (tomou a vacina? boolean)
 */
 
@@ -23,12 +23,22 @@ Vacina (tomou a vacina? boolean)
 #include <string.h>
 #include <time.h>
 
+int strSymbol(char string[30], int size){
+
+    int i;
+    for(i=0;i<size;i++){
+        if(string[i] == '@'){
+            return 1;
+        }
+    }
+};
+
 int main(void)
 {
-    
+
     srand(time(NULL));
 
-    int size, i, j=0, check = 0;
+    int size, i, j, check = 0;
 
     do
     {
@@ -36,26 +46,44 @@ int main(void)
         scanf("%d", &size);
 
     } while (size < 1 || size > 1000);
-    
-        
 
-    int id[size][1];
-    char nome[size][30], email[size][30], sexo[size][15], endereco[size][50], vax;
-    double altura[size][1];
-    bool vacina[size][5];
+
+
+    int id[size], emailCheck = 0;
+    char nome[size][30], email[size][30], sexo[size][15], endereco[size][50], vax[size];
+    double altura[size];
+    bool vacina[size];
 
     fflush(stdin);
 
     for(i=0; i<size; i++){
 
-        id[i][i] = rand() % 999+1; 
+        id[i] = rand() % 999+1;
 
-        printf("Informe o nome do usuario ID-%d: ", id[i][i]);
+        printf("Informe o nome do usuario ID-%d: ", id[i]);
         fgets(nome[i], 30, stdin);
-   
+
         printf("Informe o email do usuario: ");
         fgets(email[i], 30, stdin);
- 
+
+        do{
+
+            printf("Informe o email do usuario: ");
+            fgets(email[i], 30, stdin);
+
+            for(j=0;email[i][j] != '\0';j++){
+
+                if(email[i][j] == '@'){
+                    emailCheck = 1;
+                }
+
+            }
+            if(emailCheck == 0){
+                    printf("!!O emailo informado e invalido!!\n");
+            }
+
+        }while(emailCheck == 0);
+
 
         printf("Informe o sexo do usuario: ");
         fgets(sexo[i], 30, stdin);
@@ -63,30 +91,43 @@ int main(void)
         printf("Informe o endereco do usuario: ");
         fgets(endereco[i], 30, stdin);
 
-        printf("Informe a altura do usuario: ");
-        scanf("%f", &altura);
+        do{
+            printf("Informe a altura do usuario: ");
+            scanf("%lf", &altura[i]);
+
+            if(altura[i] < 1 || altura[i] > 2){
+
+                printf("!!Altura informada invalida, informe valores entre 1m e 2m!!\n");
+            }
+
+        }while(altura[i] < 1 || altura[i] > 2);
+
 
         fflush(stdin);
 
         printf("O usuario esta vacinado?: ");
-        scanf("%c", &vax);
+        scanf("%c", &vax[i]);
 
         fflush(stdin);
-        
+
         printf("\n");
     }
 
     printf("\n");
 
     for(i=0; i<size; i++){
-        printf("%s", nome[i]); 
-        printf("%s", email[i]);
-        printf("%s", sexo[i]);
-        printf("%s", endereco[i]);  
+
+        printf("-----USUARIO %d-----\n", id[i]);
+        printf("\nNome: %s", nome[i]);
+        printf("Email: %s", email[i]);
+        printf("Sexo: %s", sexo[i]);
+        printf("Endereco: %s", endereco[i]);
+        printf("Altura: %.2lf\n", altura[i]);
+        printf("Esta vacinado?: %c", vax[i]);
 
         printf("\n");
     }
 
-    
+
     return 0;
 }
